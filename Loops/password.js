bc2b2199adeda423e710f7ae3d4f90d064ae7e29
Brian Shewan <brian.shewan@nscc.ca>
2021-10-05 10:21:29 -0300

    added loop examples

---
+++ Loops/password.js
@@ -0,0 +1,9 @@
+
+var prompt = require("prompt-sync")()
+
+// ask a user for their password
+var password = ""
+while (password === "") {
+    password = prompt("Enter a password: ")
+}
+console.log("Yeah! You finally typed it in!")
