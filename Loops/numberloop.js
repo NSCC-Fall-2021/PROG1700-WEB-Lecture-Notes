0817772be29421af843ea05d6fb2c9fef49f0da9
Brian Shewan <brian.shewan@nscc.ca>
2021-10-15 10:21:52 -0300

    added array examples

--- Loops/numberloop.js
+++ Loops/numberloop.js
@@ -1,12 +1,7 @@
 var prompt = require('prompt-sync')()
 
 // ask the user to type in numbers until they are done
-var num = 0
-var end = ""
-while (end != "Y") {
-    end = prompt("Enter 'Y' to enter a number: ")
-    if (end == "Y") {
-        num = prompt("Enter a number: ")
-        end = ""
-    }
-}
+do {
+    var num = prompt("Enter a number: ")
+    var end = prompt("Enter 'Y' to enter a number: ")
+} while (end == "Y")
